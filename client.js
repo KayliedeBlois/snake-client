8b423be1f47d06ecd066144e08264595a1633cb7
KayliedeBlois <kayliedeblois@gmail.com>
2022-07-06 14:31:35 -0400

    Tested movements using setTimeout and setInterval

--- client.js
+++ client.js
@@ -18,6 +18,53 @@ const connect = function () {
     conn.write("Name: KWD");
   });
 
+  conn.on("connect", () => {
+    console.log("Move: up");
+  });
+
+  // TEST = Moves up continually every 50 ms
+  // setInterval(() => {
+  //   console.log("Move: up");
+  // }, 50); 
+
+  // TEST = Moves up, right, down, down, left changes every 50 ms
+  // conn.on("connect", () => {
+  //   let time = 0;
+  //   setTimeout(() => {
+  //     console.log("Move: up");
+  //   }, time)
+  //   time += 50;
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     console.log("Move: right");
+  //   }, 50); 
+  // });
+
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     console.log("Move: down");
+  //   }, 100); 
+  // });
+
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     console.log("Move: down");
+  //   }, 150); 
+  // });
+
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     console.log("Move: left");
+  //   }, 150); 
+  // });
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
