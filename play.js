9814cb8621326bc1ff373671d3bc27159b33d00e
KayliedeBlois <kayliedeblois@gmail.com>
2022-07-06 13:58:57 -0400

    Added client and play files for snake game

---
+++ play.js
@@ -0,0 +1,9 @@
+const net = require("net");
+// establishes a connection with the game server
+
+const connect = require("./client");
+// uses connect function from client.js
+
+
+console.log("Connecting ...");
+connect();
